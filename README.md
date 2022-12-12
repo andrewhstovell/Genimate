65eb56f95292fe3071be9041bef683ce54d06861
Andrew Stovell <andrewhstovell@gmail.com>
2022-12-12 15:00:50 +1030

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# Genimate
-A simple tool for creating generative animated art of any size.
+v1.0.0
+
+# Genimate: A tool to create Generative Animations
+This script can be used to assemble generative animated images using weighted components,
+and create the metadata in the format required by [MintGarden.io](mintgarden.io)'s
+MintGarden Studio bulk minter. It makes use of [Gifski](https://gif.ski/) an amazing lossless GIF creation tool.
+
+I've tried to make this as beginner friendly as possible.
+
+This is an extension of my [NFT-Assembler-for-MintGarden-Bulk](https://github.com/andrewhstovell/Chia-NFT-Assembler-for-MintGarden-Bulk) example,
+With the main difference being that this is intended to produce animated images.
+
+#### Disclaimer:
+* I am not affiliated with MintGarden.io
+* This software is provided as is, for example purposes.
+
+
+## Dependancies
+- [Python 3](https://www.python.org/downloads/)
+- [Pillow 9.x](https://pillow.readthedocs.io/en/stable/) run: `pip install Pillow`
+
+## Running the Example
+1. Clone the repository to your device, or [download the folder](https://github.com/andrewhstovell/Genimate/archive/refs/heads/main.zip) and extract the contents.
+2. Navigate to the directory in your CLI (or `right-click + open terminal here` in the folder on Windows)
+3. Type/paste `Done!" when done
+
+![screenshot of steps 2 to 3]() TO DO
+
+4. Check the newly created ***./collection/*** folder and contents that have been created. Remember to look at the metadata file.
+
+> ***In the name of all that is good, please don't try to turn these example images into NFT's.***
+
+## Usage
+1. Create your own component PNG frames and place them in ***./components/TraitName/TraitVariation*** directory, and name them 1.png -> X.png in chronological order
+
+For example: ./components/Feature/Blackhole/1.png
+
+    *Your components can use any dimensions, as long as they are all the same.*
+
+2. Edit the `genimate.py` file, set your collection's name, description and the amount of images to generate, the FPS and Frames per Animation configs
+```
+# Change these
+COLLECTION_NAME = "Super Duper Animated Example Collection"
+COLLECTION_DESCRIPTION = "Super Duper Animated Example NFT's"
+TOTAL_IMAGES = 100 # Number of random unique images you want to generate
+FRAMES_PER_ANIMATION = 3 # The number of frames that have been created for each trait variant
+FPS = 5 # The speed of the Animation, the lower = slower
+```
+3. Replace the component names with the names of your TraitName directories
+4. Adjust corresponding weights accordingly (lower = rarer)
+```
+# The weightings for each trait drive the rarity and add up to 100%
+traits = [
+    Trait(
+        "Background", # Parent Folder
+        ["Pink", "Teal", "Lime", "Cream"], # Sub Folder
+        [30, 20, 10, 40]),
+    Trait(
+        "Feature", 
+        ["House", "Tree", "Sun", "Moon"], 
+        [15, 35, 35, 15]),
+    Trait(
+        "Face", 
+        ["Round", "Square", "Triangle"], 
+        [30, 50, 20]),
+    Trait(
+        "Eyes", 
+        ["Blue", "Red", "Brown"], 
+        [30, 10, 60]),
+    Trait(
+        "Nose", 
+        ["Pointy", "Rounded", "Dots"], 
+        [30 , 30 , 40]),
+    Trait(
+        "Mouth", 
+        ["Smile", "Grumpy", "Neutral", "Cute"], 
+        [30, 30, 20, 20]),
+]
+```
+* Run the program, and generate your Images and Metadata
+* Run the program again until your batch of NFT's is just right!
+
+## Testing
+This is especially important if you have edited the code beyond what is presented in these instructions.
+
+To double-check that every NFT has unique trait assignments in the metadata, 
+run `python duplicate_identifier.py`
+
+You should get 0 duplicates identified.
+
+### Problems or need help?
+Feel free to create an issue ticket, i will try to address as soon as possible
+
+----
+> *Want to toss some Mojo's or an NFT at the Andy?* <br/>
+`xch1pelfpwqtnn0waj6vkrvdn7v8cx7h93gjmknakhq58hhrnlhqmk9s8xv64r`
---
+++ genimate.py
@@ -0,0 +1,214 @@
+from PIL import Image, ImageDraw, ImageFont
+from IPython.display import display
+from shutil import rmtree
+from os import path, mkdir, listdir, system
+from dataclasses import dataclass
+import sys
+import csv
+import random
+import subprocess
+
+## CONFIGURATION
+# Change these
+COLLECTION_NAME = "Super Duper Animated Example Collection"
+COLLECTION_DESCRIPTION = "Super Duper Animated Example NFT's"
+TOTAL_IMAGES = 100 # Number of random unique images you want to generate
+FRAMES_PER_ANIMATION = 3 # The number of frames that have been created for each trait variant
+FPS = 5 # The speed of the Animation, the lower = slower
+
+# Should not need to change these
+TEMP_DIR = './temp' # This directory will temporarily store the constructed frames for the animation
+COLLECTION_DIR = './collection' # Output directory
+COMPONENT_DIR = './components' # This is the directory where your Trait directories reside
+
+all_images = [] # This is used to store the images as they are generated
+
+# Each image is made up a series of traits
+@dataclass
+class Trait:
+    name: str
+    variants: list[str]
+    weights: list[int]
+
+# Make sure these Trait Types and Names match the directory structure of the traits. Case-Sensitive
+
+# FOR EXAMPLE
+# components/TraitType/Trait/x.png
+# components/Background/Pink/x.png
+# Where x is the frame number.
+
+# The weightings for each trait drive the rarity and add up to 100%
+# Note traits in this list must be in order of Layer. I.e., Background first, Foreground last.
+traits = [
+    Trait(
+        "Background", 
+        ["Pink", "Teal", "Lime", "Cream"], 
+        [30, 20, 10, 40]),
+    Trait(
+        "Feature", 
+        ["House", "Tree", "Sun", "Moon"], 
+        [15, 35, 35, 15]),
+    Trait(
+        "Face", 
+        ["Round", "Square", "Triangle"], 
+        [30, 50, 20]),
+    Trait(
+        "Eyes", 
+        ["Blue", "Red", "Brown"], 
+        [30, 10, 60]),
+    Trait(
+        "Nose", 
+        ["Pointy", "Rounded", "Dots"], 
+        [30 , 30 , 40]),
+    Trait(
+        "Mouth", 
+        ["Smile", "Grumpy", "Neutral", "Cute"], 
+        [30, 30, 20, 20]),
+]
+
+
+
+## Generate Traits
+# A recursive function to generate unique trait combinations (i.e., an image)
+def create_new_image():
+    new_image = {}
+    
+    # For each trait category, select a random trait based on the weightings
+    for trait in traits:
+        new_image[trait.name] = random.choices(trait.variants, trait.weights)[0]
+    
+    if (new_image in all_images):
+        return create_new_image()
+    else:
+        return new_image
+
+
+
+## Helper function for generating progress bars    
+def progress_bar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '#', print_end = "\r"):
+    total = len(iterable)
+    
+    # Progress Bar Printing Function
+    def print_progress_bar (iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filled_length = int(length * iteration // total)
+        bar = fill * filled_length + '-' * (length - filled_length)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = print_end)
+        
+    # Initial Call
+    print_progress_bar(0)
+    
+    # Update Progress Bar
+    for i, item in enumerate(iterable):
+        yield item
+        print_progress_bar(i + 1)
+    print()
+
+
+# Generate the unique combinations based on trait weightings
+for i in progress_bar(range(TOTAL_IMAGES), prefix = 'Combining Images:', suffix = 'Complete', length = 25):
+    new_trait_image = create_new_image()
+    all_images.append(new_trait_image)
+    
+    
+    
+## Check the stats of the new images
+# Returns true if all images are unique
+def all_images_unique(all_images):
+    seen = list()
+    return not any(i in seen or seen.append(i) for i in all_images)
+
+print("Are all images unique? %s" % (all_images_unique(all_images)))
+
+def make_empty_directory(dir : str):
+    if path.isdir(dir):
+        rmtree(dir)
+    mkdir(dir)
+    
+make_empty_directory(COLLECTION_DIR)
+
+def generate_frames_from_traits(image):
+    for i in range(1, FRAMES_PER_ANIMATION + 1):
+        layers = []
+
+        # Load the components frames which will make up the image
+        for trait in image:
+            layers.append(Image.open(f'{COMPONENT_DIR}/{trait}/{image[trait]}/{i}.png').convert('RGBA'))
+
+        # Merge the frames layers
+        composite = Image.alpha_composite(layers[0], layers[1])
+        for img_no in range(1, len(layers)):
+            composite = Image.alpha_composite(composite, layers[img_no])
+
+        # Convert to RGB & save the frame
+        rgb_im = composite.convert('RGB')
+        rgb_im.save(f'{TEMP_DIR}/{i}.png')
+        
+        
+        
+        
+def create_lossless_gif(directory: str, gif_filename):
+    """
+    Create a lossless GIF using gifski from a directory containing PNG images.
+
+    Args:
+        directory: The directory containing the PNG images.
+        gif_filename: The name of the output GIF file.
+
+    Returns:
+        None
+    """
+    # Get a list of the files in the directory, and filter for only PNG files
+    files = [f for f in listdir(directory) if f.endswith('.png')]
+
+    # Sort the list of files numerically
+    files = sorted(files, key=lambda x: int(x.split('.')[0]))
+
+    # Open the first file to determine the dimensions of the GIF
+    first_file = f'{directory}/{files[0]}'
+    with Image.open(first_file) as image:
+        width, height = image.size
+
+    # Build the command to run gifski - This command is generally less secure than
+
+    gifski_command = f'gifski.exe --width {width} --height {height} --fps {FPS} --quality 100 --quiet --output=\"{COLLECTION_DIR}/{gif_filename}\" {directory}/*.png'
+    
+    # Run the gifski command
+    result = system(gifski_command)
+
+
+
+## Generate Animations and Metadata (metadata is suitable for MintGarden Studio bulk minter)
+# Add the file, name and description for each image
+for i in progress_bar(range(len(all_images)), prefix = 'Generating Animations and Metadata:', suffix = 'Complete', length = 25):
+    name = "%s #%s" % (COLLECTION_NAME, str(i + 1))
+    make_empty_directory(TEMP_DIR)
+    generate_frames_from_traits(all_images[i])
+    create_lossless_gif(TEMP_DIR, f'{name}.gif')
+    
+    # File metadata to suit MintGarden Bulk Generator
+    # Feel free to change the values, do not change the keys.
+    file_data = {
+        "file": "%s.gif" % (str(i + 1)),                        
+        "name": name,
+        "description": "%s/%s %s" % (str(i + 1), str(TOTAL_IMAGES), COLLECTION_DESCRIPTION)  
+    }
+    file_data.update(all_images[i])
+    all_images[i] = file_data
+    
+rmtree(TEMP_DIR)
+    
+# Create the metadata.csv file ready for the MintGarden Bulk minter
+metadata_file = open(f'./{COLLECTION_DIR}/metadata.csv', 'w', newline='')
+writer = csv.writer(metadata_file, delimiter =';')
+
+# Write the metadata headers
+writer.writerow(all_images[0].keys())
+
+for item in progress_bar(all_images, prefix = 'Compiling Metadata:', suffix = 'Complete', length = 25):
+    # Write the metadata for this item to metadata.csv
+    writer.writerow(item.values())
+
+metadata_file.close()
+
+print("DONE!")
